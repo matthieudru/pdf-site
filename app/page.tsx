d5e4e6427e57dd8788a044df3904cd868f80af86
Matthieu Dru <matthieudru@macbookpro.home>
2026-03-31 22:43:30 +0200

    Nouveau PDF avec nom original

--- app/page.tsx
+++ app/page.tsx
@@ -9,14 +9,14 @@ export default function Home() {
       /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
 
     if (isMobile || isSafari) {
-      window.location.href = "/document.pdf";
+      window.location.href = "/Outil_parents_protection_enfants_2026_versiondef.pdf";
     }
   }, []);
 
   return (
     <main className="h-screen w-screen overflow-hidden">
       <iframe
-        src="/document.pdf"
+        src="/Outil_parents_protection_enfants_2026_versiondef.pdf"
         className="w-full h-full border-0"
         title="Document preview"
       />
